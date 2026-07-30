23369d21a7444e727cf10d79f86cf7b403a1efcc
Praisejah Nwabeke <nwabekepraisejah@gmail.com>
2026-07-30 14:01:58 +0100

    Added db cert

--- agent-backend/database/db.py
+++ agent-backend/database/db.py
@@ -19,7 +19,7 @@ class CareerCopilotDB:
 
     def connect(self):
         logger.info("Connecting to database")
-        self.conn = psycopg2.connect(self.database_url)
+        self.conn = psycopg2.connect(self.database_url, sslmode="verify-full",sslrootcert="/var/task/certs/root.crt",)
         register_vector(self.conn)
         logger.info("Successfully connected to database")
         return self
